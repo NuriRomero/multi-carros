fc7216526ecd7ca268ed62e1521ced104956f51c
NuriRomero <nuri.cruz@devitm.com>
2023-10-09 10:27:11 -0600

    Fixed cars-grid

--- inc/cars-filters.php
+++ inc/cars-filters.php
@@ -1,5 +1,5 @@
 <?php
-
+//Function to filter cars
 function filtrar_por_categoria() {
 	$selected_brand = isset($_POST['cars-brand-selector']) ? $_POST['cars-brand-selector'] : 'Todas las marcas';
 	$selected_fuel = isset($_POST['cars-fuel-selector']) ? $_POST['cars-fuel-selector'] : 'Todos los tipos de combustible';
